dad202b900d74ff1836c183d501043bc5bb2b26b
EsmaeelNabil <esmaeel.moustafa@zenjob.com>
2023-01-17 22:56:01 +0100

    Initial Commit

---
+++ rmwall/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  rmwall
+//
+//  Created by Esmaeel Nabil Mohamed Moustafa on 17.01.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rmwall/rmwallApp.swift
@@ -0,0 +1,17 @@
+//
+//  rmwallApp.swift
+//  rmwall
+//
+//  Created by Esmaeel Nabil Mohamed Moustafa on 17.01.23.
+//
+
+import SwiftUI
+
+@main
+struct rmwallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
